834947240426b43b24c9bb8e0713c24f399553f7
bqqbarbhg <samuli.1995@hotmail.com>
2015-11-25 16:24:13 +0200

    Fix date format

--- src/util/DateUtils.swift
+++ src/util/DateUtils.swift
@@ -3,7 +3,7 @@ import Foundation
 var iso8601DateFormatter: NSDateFormatter = {
     let dateFormatter = NSDateFormatter()
     dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
+    dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSSxxx"
 
     return dateFormatter
 }()
