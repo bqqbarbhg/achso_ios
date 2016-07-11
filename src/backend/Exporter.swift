45d3ec5276e48f8ddaac2e57f1f477d128bdf01e
Matti Jokitulppo <melonmanchan@gmail.com>
2016-07-11 12:25:41 +0300

    Add callback support for exporting videos

--- src/backend/Exporter.swift
+++ src/backend/Exporter.swift
@@ -9,7 +9,7 @@ class VideoExporter {
         self.endpoint = endpoint
     }
     
-    func exportVideos(videos: [Video], email: String) {
+    func exportVideos(videos: [Video], email: String, exportCallback: (error: NSError?, message: String?) -> Void) {
         
         var videosAsJson = [JSONObject]()
         
@@ -29,16 +29,32 @@ class VideoExporter {
             }
             
             let request = self.endpoint.request(.POST, "/", json: jsonObject)
-            http.authorizedRequestJSON(request, canRetry: false) { response in
+            http.authorizedRequestJSON(request, canRetry: true) { response in
                 switch response.result {
                 case .Failure(let error):
-                    NSLog(error.localizedDescription)
+                    exportCallback(error: error, message: nil)
                 case .Success(let value) :
                     do {
                         let json = try (value as? JSONObject).unwrap()
                         let message: String = try json.castGet("message")
-                        NSLog(message)
+                        // Alamofire considers all finished requests as being succesful,
+                        // therefore, a status code check is in order here.
+                        if let statusCode = response.response?.statusCode {
+                            if statusCode != 201 {
+                                
+                                let error = NSError(domain: "com.legroup.achso!", code: -1, userInfo: [
+                                    NSLocalizedDescriptionKey: message])
+                                
+                                exportCallback(error: error, message: nil)
+                            }
+                        } else {
+                            exportCallback(error: nil, message: message)
+                        }
                     } catch {
+                        let error = NSError(domain: "com.legroup.achso!", code: -1, userInfo: [
+                            NSLocalizedDescriptionKey: "Parsing JSON response failed!" ])
+                        
+                        exportCallback(error: error, message: nil)
                     }
                 }
             }
